641a1f0b718753e5c61135d5765a772f7ed0e29d
mt <mt@gmail.com>
2025-03-02 22:45:53 +0800

    a

--- schools/chain.go
+++ schools/chain.go
@@ -10,12 +10,12 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
+	"time"
 )
 
-var lock = make(chan struct{}, 1)
-
 func main() {
-	wsURL := "wss://mainnet.gateway.tenderly.co"
+	ctx := context.Background()
+	wsURL := "wss://ethereum-rpc.publicnode.com"
 	client, err := ethclient.Dial(wsURL)
 	if err != nil {
 		log.Fatalf("无法连接到 WebSocket 节点: %v", err)
@@ -25,6 +25,11 @@ func main() {
 	number, _ := client.BlockNumber(context.Background())
 	fmt.Printf("当前区块高度：%d\n", number)
 
+	var lastTime time.Time
+	newBlockLog := make(chan *types.Header, 2048)
+	newBlockSub, _ := client.SubscribeNewHead(ctx, newBlockLog)
+	defer newBlockSub.Unsubscribe()
+
 	contractAddress := common.HexToAddress("0xdac17f958d2ee523a2206206994597c13d831ec7")
 	topic := crypto.Keccak256Hash([]byte("Transfer(address,address,uint256)"))
 	query := ethereum.FilterQuery{
@@ -33,19 +38,26 @@ func main() {
 	}
 
 	logs := make(chan types.Log, 100)
-	ctx := context.Background()
 	sub, err := client.SubscribeFilterLogs(ctx, query, logs)
 	if err != nil {
 		log.Fatalf("订阅失败: %v", err)
 	}
 	defer sub.Unsubscribe()
 
-	fmt.Println("开始监听 USDT Transfer 事件...")
 	for {
 		select {
 		case err := <-sub.Err():
 			log.Printf("订阅错误: %v", err)
 			return
+		case newBlock := <-newBlockLog:
+			currentTime := time.Unix(int64(newBlock.Time), 0)
+			if !lastTime.IsZero() {
+				timeDiff := currentTime.Sub(lastTime)
+				fmt.Printf("Block #%d, Hash: %s, Time: %s\n", newBlock.Number, newBlock.Hash().Hex(), currentTime)
+				fmt.Printf("Time since last block: %s\n", timeDiff)
+			}
+			fmt.Printf("区块高度:%d\n", newBlock.Number)
+			lastTime = currentTime
 		case vLog := <-logs:
 			if len(vLog.Topics) == 3 {
 				value := new(big.Int).SetBytes(vLog.Data).Int64() / 1000000
